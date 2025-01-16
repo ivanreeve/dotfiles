493fe5f59ab28c984071e6d88de36184d17e671c
ivanreeve <ivanreeve@gmail.com>
2025-01-16 13:28:46 +0800

    rm nvin

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # TODO: some configs are still missing
 
@@ -56,11 +56,3 @@ rm -rf ../.cache/oh-my-posh/themes/
 mkdir -p ../.cache/oh-my-posh/themes/
 cp -f ~/.cache/oh-my-posh/themes/current.omp.json ../.cache/oh-my-posh/themes/
 show_log "oh-my-posh"
-
-# Sync neovim
-rm -rf ../.config/nvim/
-mkdir -p ../.config/nvim/
-cp -rf ~/.config/nvim/* ../.config/nvim
-mkdir -p ../.config/nvim/lua/plugins/alpha-themes/
-cp -rf ~/.local/share/nvim/lazy/alpha-nvim/lua/alpha/themes/* ../.config/nvim/lua/plugins/alpha-themes/
-show_log "neovim"
